4bcd140b39508b64546202d7877422ff8387310d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-09 16:43:45 +0100

    Monkey-patch tree rng to add 'colors'
    'colors' has been removed from the rng schema in 13.0.
    Add it in the XML before loading the schema.
    We should prefer another solution which does not involve a monkey-patch
    that will work only for one module doing it.

--- web_tree_dynamic_colored_field/__init__.py
+++ web_tree_dynamic_colored_field/__init__.py
@@ -1,1 +1,33 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import os
+from lxml import etree
+
+from odoo import tools
+from odoo.tools import view_validation
+
+
+orig_relaxng = view_validation.relaxng
+
+
+def relaxng_colors(view_type):
+    if view_type == 'tree':
+        with tools.file_open(os.path.join('base', 'rng', '%s_view.rng' % view_type)) as frng:
+            relaxng_doc = etree.parse(frng)
+            namespaces = relaxng_doc.getroot().nsmap
+            tree_doc = relaxng_doc.xpath('//rng:element[@name="tree"]', namespaces=namespaces)[0]
+            optional = etree.SubElement(
+                tree_doc,
+                "{http://relaxng.org/ns/structure/1.0}optional"
+            )
+            etree.SubElement(
+                optional,
+                "{http://relaxng.org/ns/structure/1.0}attribute",
+                name="colors"
+            )
+            view_validation._relaxng_cache[view_type] = etree.RelaxNG(relaxng_doc)
+    else:
+        orig_relaxng(view_type)
+
+
+view_validation.relaxng = relaxng_colors
